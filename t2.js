3a262ce3b170f33cca9102b98535c8dba64709a5
Christopher J. Brody <brodybits@litehelpers.net>
2015-10-06 00:46:03 +0200

    Static path & Javascript CB working on Node 0.12 [XXX TODO FIX LEAK]

--- t2.js
+++ t2.js
@@ -1,5 +1,26 @@
 var i = require('./');
 
-setTimeout(function() {
-i.newEventServer().newHTTPServer().uvtest();
-}, 10);
+//setTimeout(function() {
+//i.newEventServer().newHTTPServer().uvtest();
+//}, 10);
+
+//i.newHTTPServer().uvtest();
+var myhost = "0.0.0.0",
+    myport = 8080;
+
+//i.newHTTPServer().bindAddr(myhost, myport, 111);
+
+var httpServer = i.newHTTPServer();
+httpServer.bindAddr(myhost, myport, 111);
+httpServer.staticPath("/", 200, "Content from root path\n");
+httpServer.pathCB("/test", function(r) {
+  console.log('asdf');
+  r.res(200, 'Response from /test Javascript callback\n');
+});
+
+httpServer.pathCB("/test2", function(r) {
+  console.log('asdf');
+  setTimeout(function() {
+    r.res(200, 'Response from /test2 Javascript callback\n');
+  }, 0)
+});
